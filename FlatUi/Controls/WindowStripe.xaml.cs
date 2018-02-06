7a893c8a3656e9e21f45cae5a63ef4c3154b57e1
Тарас Димкар <sarat@epicentrk.ua>
2018-02-06 14:29:00 +0200

    Initial

---
+++ FlatUi/Controls/WindowStripe.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace FlatUi.Controls
+{
+    public partial class WindowStripe
+    {
+        public WindowStripe()
+        {
+            InitializeComponent();
+        }
+
+        private void Rectangle_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                Window.GetWindow(this)?.DragMove();
+        }
+
+        private void BtnCloseClick(object sender, RoutedEventArgs e)
+        {
+            Window.GetWindow(this)?.Close();
+        }
+
+        private void BtnMaxClick(object sender, RoutedEventArgs e)
+        {
+            var wnd = Window.GetWindow(this);
+            if (wnd == null) return;
+
+            if (wnd.WindowState != WindowState.Maximized)
+            {
+                wnd.WindowState = WindowState.Maximized;
+                return;
+            }
+            if (wnd.WindowState != WindowState.Normal)
+            {
+                wnd.WindowState = WindowState.Normal;
+                return;
+            }
+        }
+
+        private void BtnMinClick(object sender, RoutedEventArgs e)
+        {
+            var wnd = Window.GetWindow(this);
+            if (wnd == null) return;
+
+            if (wnd.WindowState != WindowState.Minimized)
+            {
+                wnd.WindowState = WindowState.Minimized;
+                return;
+            }
+            if (wnd.WindowState != WindowState.Normal)
+            {
+                wnd.WindowState = WindowState.Normal;
+                return;
+            }
+        }
+    }
+}
---
+++ FlatUi/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace FlatUi
+{
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var gridItems = new ObservableCollection<Student>();
+            var rand = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                gridItems.Add(new Student(i, "Student"+i, rand.Next(99999999).ToString()));
+            }
+
+            DataGrid.ItemsSource = gridItems;
+        }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public bool Married { get; set; }
+
+        public Student(int id, string name, string phone)
+        {
+            Id = id;
+            Name = name;
+            Phone = phone;
+            Married = false;
+        }
+    }
+}
